22854a68b770885c617a1652d5037313caea15f0
lake <lake1310@gmail.com>
2020-08-12 02:28:59 +0900

    cookbook

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,182 @@
 import 'package:flutter/material.dart';
 
-//snackbar
+//work with tabs
+void main() {
+  runApp(TabBarDemo());
+}
+
+class TabBarDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
+            title: Text('Tabs Demo'),
+          ),
+          body: TabBarView(
+            children: [
+              FirstPage(),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Center'),
+    );
+  }
+}
+
+/*
+//use themes to share colors and font styles
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      theme: ThemeData(
+        // Define the default brightness and colors.
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.cyan[600],
+
+        // Define the default font family.
+        fontFamily: 'Georgia',
+
+        // Define the default TextTheme. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 52.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+      ),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome to Flutter'),
+      ),
+      body: Center(
+        child: Container(
+          //부모 테마 물려받음
+          color: Theme.of(context).accentColor,
+          child: Text(
+            'Hello World',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+        ),
+      ),
+      floatingActionButton: Theme(
+        //부모 속성 안받음
+        // data: ThemeData(
+        //   accentColor: Colors.yellow,
+        // ),
+        //부모 속성 테마를 물려받음 (secondary: 속성만 빼고)
+        data: Theme.of(context).copyWith(
+          colorScheme:
+              Theme.of(context).colorScheme.copyWith(secondary: Colors.yellow),
+        ),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add_circle_outline),
+        ),
+      ),
+    );
+  }
+}
+*/
+
+/*
+//update the ui based on orientation
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appTitle = 'Orientation Demo';
+
+    return MaterialApp(
+      title: appTitle,
+      theme: ThemeData(fontFamily: 'Georgia'), //해당 앱의 기본 폰트설정
+      home: OrientationList(
+        title: appTitle,
+      ),
+    );
+  }
+}
+
+class OrientationList extends StatelessWidget {
+  final String title;
+
+  OrientationList({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return GridView.count(
+            // Create a grid with 2 columns in portrait mode, or 3 columns in
+            // landscape mode.
+            crossAxisCount: orientation == Orientation.portrait ? 2 : 5,
+            // Generate 100 widgets that display their index in the List.
+            children: List.generate(100, (index) {
+              return Center(
+                child: Text('Item $index',
+                    // style: Theme.of(context).textTheme.bodyText1,
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontFamily: 'NanumPenScript', //특정영역만 폰트 설정
+                    )),
+              );
+            }),
+          );
+        },
+      ),
+    );
+  }
+}
+*/
+
+/*
+//display a snackbar
 void main() => runApp(SnackBarDemo());
 
 class SnackBarDemo extends StatelessWidget {
@@ -65,9 +241,10 @@ class SnackBarPage extends StatelessWidget {
     );
   }
 }
+*/
 
 /*
-// Drawer
+// add a Drawer to a screen
 void main() => runApp(CookBook());
 
 //stles 자동완성
